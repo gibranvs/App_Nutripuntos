bb47c6a243b025eff1b34c2f642d4dcbf6750637
elbaltezar <g.baltezhell@hotmail.com>
2019-08-21 16:34:33 -0500

    Update recetas.dart

--- lib/pages/recetas.dart
+++ lib/pages/recetas.dart
@@ -88,22 +88,7 @@ class _RecetasPageState extends State<RecetasPage> {
           }).toList(),
         ),
       ),
-      /*
-      new Container(
-        decoration: new BoxDecoration(
-          color: const Color(0x00FFCC00),
-          image: new DecorationImage(
-            image: new AssetImage("assets/images/fondo.jpg"),
-            colorFilter: new ColorFilter.mode(
-                Colors.black.withOpacity(0.2), BlendMode.dstATop),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Image.asset("assets/images/gr_recetas.png"),
-        ),
-      ),
-      */
+
     );
   }
 }
